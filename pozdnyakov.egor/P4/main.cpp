7a0d2dcf2b0cbcccd065b3b5fe464255254c68c9
Egor <eip_spb@mail.ru>
2025-11-22 20:50:57 +0300

    Добавил функцию копирования строки

--- pozdnyakov.egor/P4/main.cpp
+++ pozdnyakov.egor/P4/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
+#include <cstdlib>
 
 namespace pozdnyakov {
 
   size_t string_length(const char* str) {
     const char* p = str;
-    while (*p != '\0') {
+    while (*p) {
       ++p;
     }
     return p - str;
+  }
+
+  void string_copy(const char* source, char* destination, size_t dest_size) {
+    if (source == nullptr || destination == nullptr || dest_size == 0) {
+      return;
+    }
+
+    size_t i = 0;
+    while (source[i] && i < dest_size - 1) {
+      destination[i] = source[i];
+      ++i;
+    }
+    destination[i] = '\0';
   }
 }
