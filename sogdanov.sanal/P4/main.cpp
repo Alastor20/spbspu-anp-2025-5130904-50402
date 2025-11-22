23f99cab4fbd4238e8b12b6200b7b1f7e0f021e2
kuzRK <sanalsogdanov11188@gmail.com>
2025-11-22 19:36:13 +0300

    fixed bugs

--- sogdanov.sanal/P4/main.cpp
+++ sogdanov.sanal/P4/main.cpp
@@ -50,12 +50,20 @@ namespace sogdanov
     ++size;
     return str;
   }
-  char * excSnd(char * str, const char * mask, char * res)
+  size_t len(const char * a)
+  {
+    size_t count = 0;
+    for (size_t i = 0; a[i] != '\0'; ++i) {
+      count++;
+    }
+    return count;
+  }
+  char * excSnd(char * str, const char * mask, char * res, size_t size, size_t maskLen)
   {
     size_t k = 0;
-    for (size_t i = 0; str[i] != '\0'; ++i) {
+    for (size_t i = 0; i < size; ++i) {
       bool isFound = false;
-      for (size_t j = 0; mask[j] != '\0'; ++j) {
+      for (size_t j = 0; j < maskLen; ++j) {
         if (str[i] == mask[j]) {
           isFound = true;
           break;
@@ -69,13 +77,14 @@ namespace sogdanov
     res[k] = '\0';
     return res;
   }
-  char * rmvVow(char * str, char * res)
+  char * rmvVow(char * str, char * res, size_t size)
   {
     const char * vowels = "AEIOUaeiou";
+    size_t vowelsLen = len(vowels);
     size_t k = 0;
-    for (size_t i = 0; str[i] != '\0'; ++i) {
+    for (size_t i = 0; i < size; ++i) {
       bool isFound = false;
-      for (size_t j = 0; vowels[j] != '\0'; ++j) {
+      for (size_t j = 0; j < vowelsLen; ++j) {
         if (str[i] == vowels[j]) {
           isFound = true;
           break;
@@ -90,16 +99,18 @@ namespace sogdanov
     return res;
   }
 }
+
 int main()
 {
   try {
     size_t size = 0;
     char * str = sogdanov::getLine(std::cin, size);
     const char * mask = "abc";
+    size_t maskLen = sogdanov::len(mask);
     char * k1 = new char[size];
     char * k2 = new char[size];
-    char * res1 = sogdanov::excSnd(str, mask, k1);
-    char * res2 = sogdanov::rmvVow(str, k2);
+    char * res1 = sogdanov::excSnd(str, mask, k1, size, maskLen);
+    char * res2 = sogdanov::rmvVow(str, k2, size);
     std::cout << res1 << '\n' << res2 << '\n';
     delete[] res1;
     delete[] res2;
