cd21928b8b0f1b2d5b204021e1b085529cee2cce
kuzRK <sanalsogdanov11188@gmail.com>
2025-11-30 18:17:34 +0300

    fixed mistakes

--- sogdanov.sanal/P4/main.cpp
+++ sogdanov.sanal/P4/main.cpp
@@ -8,19 +8,19 @@ namespace sogdanov
     if (isSkipWs) {
       in >> std::noskipws;
     }
-    size_t len = 0;
+    size_t capacity = 0;
     char * str = nullptr;
     char ch;
-    while (in >> ch && ch !='\n') {
-      if (size + 1 > len) {
-        size_t newLen = len == 0 ? 16 : len * 2;
-        char * temp = new char[newLen];
+    while (in >> ch && ch != '\n') {
+      if (size + 1 > capacity) {
+        size_t newCap = capacity == 0 ? 16 : capacity * 2;
+        char * temp = new char[newCap];
         for (size_t i = 0; i < size; ++i) {
           temp[i] = str[i];
         }
         delete[] str;
         str = temp;
-        len = newLen;
+        capacity = newCap;
       }
       str[size] = ch;
       ++size;
@@ -28,38 +28,20 @@ namespace sogdanov
     if (isSkipWs) {
       in >> std::skipws;
     }
-    if (in.bad()) {
+    if (in.fail()) {
       delete[] str;
       size = 0;
-      throw std::logic_error("Input error\n");
-    }
-    if (in.eof()) {
       return nullptr;
     }
-    char * temp = new char[size + 1];
-    for (size_t i = 0; i < size; ++i) {
-      temp[i] = str[i];
-    }
-    delete[] str;
-    str = temp;
     str[size] = '\0';
-    ++size;
     return str;
   }
-  size_t len(const char * a)
-  {
-    size_t count = 0;
-    for (size_t i = 0; a[i] != '\0'; ++i) {
-      count++;
-    }
-    return count;
-  }
-  char * excSnd(char * str, const char * mask, char * res, size_t size, size_t maskLen)
+  char * excSnd(char * str, const char * mask, char * res)
   {
     size_t k = 0;
-    for (size_t i = 0; i < size; ++i) {
+    for (size_t i = 0; str[i] != '\0'; ++i) {
       bool isFound = false;
-      for (size_t j = 0; j < maskLen; ++j) {
+      for (size_t j = 0; mask[j] != '\0'; ++j) {
         if (str[i] == mask[j]) {
           isFound = true;
           break;
@@ -73,14 +55,13 @@ namespace sogdanov
     res[k] = '\0';
     return res;
   }
-  char * rmvVow(char * str, char * res, size_t size)
+  char * rmvVow(char * str, char * res)
   {
     const char * vowels = "AEIOUaeiou";
-    size_t vowelsLen = len(vowels);
     size_t k = 0;
-    for (size_t i = 0; i < size; ++i) {
+    for (size_t i = 0; str[i] != '\0'; ++i) {
       bool isFound = false;
-      for (size_t j = 0; j < vowelsLen; ++j) {
+      for (size_t j = 0; vowels[j] != '\0'; ++j) {
         if (str[i] == vowels[j]) {
           isFound = true;
           break;
@@ -105,16 +86,18 @@ int main()
       return 1;
     }
     const char * mask = "abc";
-    size_t maskLen = sogdanov::len(mask);
-    char * k1 = new char[size];
-    char * k2 = new char[size];
-    char * res1 = sogdanov::excSnd(str, mask, k1, size, maskLen);
-    char * res2 = sogdanov::rmvVow(str, k2, size);
+    char * k1 = new char[size + 1];
+    char * k2 = new char[size + 1];
+    char * res1 = sogdanov::excSnd(str, mask, k1);
+    char * res2 = sogdanov::rmvVow(str, k2);
     std::cout << res1 << '\n' << res2 << '\n';
-    delete[] res1;
-    delete[] res2;
+    delete[] k1;
+    delete[] k2;
     delete[] str;
   } catch (const std::bad_alloc &) {
+    delete[] k1;
+    delete[] k2;
+    delete[] str;
     std::cerr << "Memory allocation failed\n";
     return 1;
   }
