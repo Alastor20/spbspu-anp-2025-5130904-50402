bb3a4f934684294004d264155e2129cc7639af60
lavrentev.makar <makar.lavrentev@yandex.ru>
2025-12-10 22:48:55 +0300

    Realized rubber logic

--- lavrentev.makar/P5/main.cpp
+++ lavrentev.makar/P5/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdexcept>
+#include <cmath>
 
 namespace lavrentev
 {
@@ -12,6 +13,14 @@ namespace lavrentev
   {
     double width, height;
     p_t pos;
+  };
+
+  struct ru_t
+  {
+    p_t pos;
+    p_t outCenter;
+    double rPos;
+    double rOut;
   };
 
   struct Shape
@@ -30,6 +39,19 @@ namespace lavrentev
       r_t data;
     public:
       explicit Rectangle(const r_t& dd);
+      double getArea() const override;
+      lavrentev::r_t getFrameRect() const override;
+      void move(const lavrentev::p_t& c) override;
+      void move(double d_x, double d_y) override;
+      void scale(double coef) override;
+  };
+
+  struct Rubber: Shape
+  {
+    private:
+      ru_t data;
+    public:
+      explicit Rubber(const ru_t& dd);
       double getArea() const override;
       lavrentev::r_t getFrameRect() const override;
       void move(const lavrentev::p_t& c) override;
@@ -66,10 +88,10 @@ void lavrentev::Rectangle::move(const lavrentev::p_t& c)
     data.pos = c;
 }
 
-void lavrentev::Rectangle::move(double x, double y)
+void lavrentev::Rectangle::move(double d_x, double d_y)
 {
-    data.pos.x += x;
-    data.pos.y += y;
+    data.pos.x += d_x;
+    data.pos.y += d_y;
 }
 
 void lavrentev::Rectangle::scale(double coef)
@@ -80,4 +102,47 @@ void lavrentev::Rectangle::scale(double coef)
     }
     data.width *= coef;
     data.height *= coef;
+}
+
+lavrentev::Rubber::Rubber(const ru_t& dd) : data(dd)
+{
+    if(dd.rOut <= 0 || dd.rPos <= 0)
+    {
+        throw std::invalid_argument("Invalid value of radius");
+    }
+}
+
+double lavrentev::Rubber::getArea() const
+{
+    return M_PI * data.rOut * data.rOut - M_PI * data.rPos * data.rPos;
+}
+
+lavrentev::r_t lavrentev::Rubber::getFrameRect() const
+{
+    lavrentev::r_t ans;
+    ans.pos = data.outCenter;
+    ans.height = data.rOut * 2;
+    ans.width = data.rOut * 2;
+    return ans;
+}
+
+void lavrentev::Rubber::move(const lavrentev::p_t& c)
+{
+    data.pos = c;
+}
+
+void lavrentev::Rubber::move(double d_x, double d_y)
+{
+    data.pos.x += d_x;
+    data.pos.y += d_y;
+}
+
+void lavrentev::Rubber::scale(double coef)
+{
+    if (coef <= 0)
+    {
+        throw std::invalid_argument("Coef must be positive");
+    }
+    data.rPos *= coef;
+    data.rOut *= coef;
 }
