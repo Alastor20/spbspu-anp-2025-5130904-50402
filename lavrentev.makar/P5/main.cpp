e30fc6a683df396bd01313cb2be0c265f92f51d0
lavrentev.makar <makar.lavrentev@yandex.ru>
2025-12-10 22:32:04 +0300

    Realized rectangle logic

--- lavrentev.makar/P5/main.cpp
+++ lavrentev.makar/P5/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 
 namespace lavrentev
 {
@@ -33,6 +34,7 @@ namespace lavrentev
       lavrentev::r_t getFrameRect() const override;
       void move(const lavrentev::p_t& c) override;
       void move(double d_x, double d_y) override;
+      void scale(double coef) override;
   };
 }
 
@@ -41,3 +43,41 @@ int main()
 
 }
 
+lavrentev::Rectangle::Rectangle(const r_t& dd) : data(dd)
+{
+    if (dd.width <= 0 || dd.height <= 0)
+    {
+        throw std::invalid_argument("Invalid weight or height");
+    }
+}
+
+double lavrentev::Rectangle::getArea() const
+{
+    return data.width * data.height;
+}
+
+lavrentev::r_t lavrentev::Rectangle::getFrameRect() const
+{
+    return data;
+}
+
+void lavrentev::Rectangle::move(const lavrentev::p_t& c)
+{
+    data.pos = c;
+}
+
+void lavrentev::Rectangle::move(double x, double y)
+{
+    data.pos.x += x;
+    data.pos.y += y;
+}
+
+void lavrentev::Rectangle::scale(double coef)
+{
+    if (coef <= 0)
+    {
+        throw std::invalid_argument("Coef must be positive");
+    }
+    data.width *= coef;
+    data.height *= coef;
+}
