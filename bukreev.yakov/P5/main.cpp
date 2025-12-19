deaf6904bf33a40be54100c9370f8a0afed3f1bc
BukreevYas <bukreev.yas@edu.spbstu.ru>
2025-12-19 10:07:35 -0500

    Defined Shape interface

--- bukreev.yakov/P5/main.cpp
+++ bukreev.yakov/P5/main.cpp
@@ -9,6 +9,15 @@ namespace bukreev
     point_t pos;
     double width, height;
   }
+
+  struct Shape
+  {
+    virtual double getArea() const;
+    virtual rectangle_t getFrameRect() const;
+    virtual void move(point_t newPos);
+    virtual void move(double dX, double dY);
+    virtual void scale(double k);
+  }
 }
 
 int main()
