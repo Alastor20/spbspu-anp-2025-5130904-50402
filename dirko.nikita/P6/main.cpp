d91de359b4ab77c3d25b5501686c92a548478415
Alastor20 <nik_d.007@mail.ru>
2025-12-31 17:50:39 +0300

    base defenition

--- dirko.nikita/P6/main.cpp
+++ dirko.nikita/P6/main.cpp
@@ -1,5 +1,29 @@
-int main()
-{
+#include <cstddef>
+#include "../common/shapesUtil.hpp"
 
-  return 0;
+namespace dirko
+{
+  struct Shape_vec final: Shape
+  {
+    Shape_vec();
+    Shape_vec(const Shape_vec &);
+    Shape_vec(Shape_vec &&);
+    Shape_vec &operator=(const Shape_vec &);
+    Shape_vec &operator=(Shape_vec &&);
+    ~Shape_vec() noexcept;
+    double getArea() const noexcept;
+    rec_t getFrameRect() const noexcept;
+    void move(p_t point) noexcept;
+    void move(double dx, double dy) noexcept;
+    void doScale(double coef) noexcept;
+    void doScaleSafe(double coef);
+
+  private:
+    Shape *shps_;
+    size_t size_;
+    size_t cap_;
+    void scale(double coef) noexcept;
+  };
 }
+int main()
+{}
