c33048b15edbeea53cbaec32a193fcd4b0c405ad
Alastor20 <nik_d.007@mail.ru>
2026-01-03 19:53:31 +0300

    implement special methods

--- dirko.nikita/P6/main.cpp
+++ dirko.nikita/P6/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstddef>
 #include "../common/shapesUtil.hpp"
 
@@ -10,20 +11,60 @@ namespace dirko
     Shape_vec(Shape_vec &&);
     Shape_vec &operator=(const Shape_vec &);
     Shape_vec &operator=(Shape_vec &&);
-    ~Shape_vec() noexcept;
-    double getArea() const noexcept;
-    rec_t getFrameRect() const noexcept;
-    void move(p_t point) noexcept;
-    void move(double dx, double dy) noexcept;
+    ~Shape_vec() noexcept override;
+    double getArea() const noexcept override;
+    rec_t getFrameRect() const noexcept override;
+    void move(p_t point) noexcept override;
+    void move(double dx, double dy) noexcept override;
     void doScale(double coef) noexcept;
     void doScaleSafe(double coef);
 
   private:
-    Shape *shps_;
+    Shape **shps_;
     size_t size_;
-    size_t cap_;
-    void scale(double coef) noexcept;
+    void scale_(double coef) noexcept override;
   };
 }
 int main()
 {}
+
+dirko::Shape_vec::Shape_vec():
+  shps_(nullptr),
+  size_(0)
+{}
+
+dirko::Shape_vec::~Shape_vec() noexcept
+{
+  for (size_t i = 0; i < size_; ++i) {
+    delete shps_[i];
+  }
+  delete[] shps_;
+}
+
+dirko::Shape_vec::Shape_vec(const Shape_vec &other):
+  shps_(new Shape *[other.size_]),
+  size_(other.size_)
+{
+  // TODO: Still хз
+}
+
+dirko::Shape_vec::Shape_vec(Shape_vec &&r):
+  shps_(r.shps_),
+  size_(r.size_)
+{
+  r.shps_ = nullptr;
+}
+
+dirko::Shape_vec &dirko::Shape_vec::operator=(Shape_vec &&r)
+{
+  shps_ = r.shps_;
+  r.shps_ = nullptr;
+  size_ = r.size_;
+  return *this;
+}
+
+dirko::Shape_vec &dirko::Shape_vec::operator=(const Shape_vec &other)
+{
+  // TODO: (┬┬﹏┬┬)
+  return *this;
+}
