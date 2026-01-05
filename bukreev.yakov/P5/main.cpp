627d32a09aa5f975291df8029a69fc444a9fc350
BukreevYas <bukreev.yas@edu.spbstu.ru>
2026-01-05 05:05:35 -0500

    Implemented Rectangle

--- bukreev.yakov/P5/main.cpp
+++ bukreev.yakov/P5/main.cpp
@@ -3,12 +3,12 @@ namespace bukreev
   struct point_t
   {
     double x, y;
-  }
+  };
   struct rectangle_t
   {
     point_t pos;
     double width, height;
-  }
+  };
 
   struct Shape
   {
@@ -17,7 +17,42 @@ namespace bukreev
     virtual void move(point_t newPos) = 0;
     virtual void move(double dX, double dY) = 0;
     virtual void scale(double k) = 0;
-  }
+  };
+
+  struct Rectangle : Shape
+  {
+    Rectangle(point_t center, double width, double height):
+      mCenter(center),
+      mWidth(width),
+      mHeight(height)
+    {}
+    double getArea() const override
+    {
+      return mWidth * mHeight;
+    }
+    rectangle_t getFrameRect() const override
+    {
+      return rectangle_t{mCenter, mWidth, mHeight};
+    }
+    void move(point_t newPos) override
+    {
+      mCenter = newPos;
+    }
+    void move(double dX, double dY) override
+    {
+      mCenter.x += dX;
+      mCenter.y += dY;
+    }
+    void scale(double k) override
+    {
+      mWidth *= k;
+      mHeight *= k;
+    }
+
+  private:
+    point_t mCenter;
+    double mWidth, mHeight;
+  };
 }
 
 int main()
