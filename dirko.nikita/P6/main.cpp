e76ba38a87d134511a8412d79e2c8f7f9882c90c
Alastor20 <nik_d.007@mail.ru>
2026-01-06 21:07:40 +0300

    copy methods

--- dirko.nikita/P6/main.cpp
+++ dirko.nikita/P6/main.cpp
@@ -1,5 +1,6 @@
-#include <algorithm>
 #include <cstddef>
+#include <ctime>
+#include <utility>
 #include "../common/shapesUtil.hpp"
 
 namespace dirko
@@ -16,6 +17,7 @@ namespace dirko
     rec_t getFrameRect() const noexcept override;
     void move(p_t point) noexcept override;
     void move(double dx, double dy) noexcept override;
+    Shape *clone() const noexcept override;
     void doScale(double coef) noexcept;
     void doScaleSafe(double coef);
 
@@ -42,10 +44,21 @@ dirko::Shape_vec::~Shape_vec() noexcept
 }
 
 dirko::Shape_vec::Shape_vec(const Shape_vec &other):
-  shps_(new Shape *[other.size_]),
-  size_(other.size_)
+  shps_(other.size_ > 0 ? new Shape *[other.size_]() : nullptr),
+  size_(0)
 {
-  // TODO: Still хз
+  try {
+    for (size_t i = 0; i < other.size_; ++i) {
+      shps_[i] = other.shps_[i]->clone();
+      ++size_;
+    }
+  } catch (...) {
+    for (size_t i = 0; i < size_; ++i) {
+      delete shps_[i];
+    }
+    delete[] shps_;
+    throw;
+  }
 }
 
 dirko::Shape_vec::Shape_vec(Shape_vec &&r):
@@ -65,6 +78,8 @@ dirko::Shape_vec &dirko::Shape_vec::operator=(Shape_vec &&r)
 
 dirko::Shape_vec &dirko::Shape_vec::operator=(const Shape_vec &other)
 {
-  // TODO: (┬┬﹏┬┬)
+  Shape_vec tmp(other);
+  std::swap(tmp.shps_, this->shps_);
+  std::swap(tmp.size_, this->size_);
   return *this;
 }
