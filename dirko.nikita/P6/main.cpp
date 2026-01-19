a96a3f811ae8198839a5fc2683ed783c56595e9c
Alastor20 <nik_d.007@mail.ru>
2026-01-19 21:52:05 +0300

    revert lazy fix

--- dirko.nikita/P6/main.cpp
+++ dirko.nikita/P6/main.cpp
@@ -13,6 +13,7 @@ int main()
   dirko::Shape_vec shps = dirko::Shape_vec();
   shps.append(std::addressof(rec));
   shps.append(std::addressof(bub));
+  shps.append(std::addressof(pol));
   std::cout << "Before scale:\n\n";
   dirko::output(std::cout, shps.getConstIterator(), shps.size());
   dirko::point_t point = {};
