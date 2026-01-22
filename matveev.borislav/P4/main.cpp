4a76da51d951d6a198cfd07bbe2b7eacd3c4372b
L1sa620 <lisevbom@gmail.com>
2026-01-22 13:07:22 +0300

    Fix malloc

--- matveev.borislav/P4/main.cpp
+++ matveev.borislav/P4/main.cpp
@@ -14,17 +14,13 @@ int main()
 {
   size_t len = 0;
   char* str = matveev::getLine(std::cin, len);
-  if (str == nullptr) {
+  if (str == nullptr)
+  {
     std::cerr << "Error memory\n";
     return 1;
   }
 
-  size_t strLen = 0;
-  while (str[strLen] != '\0')
-  {
-    ++strLen;
-  }
-  char* res = static_cast< char* >(std::malloc(strLen + 1));
+  char* res = reinterpret_cast< char* >(std::malloc(len + 1));
   if (res == nullptr)
   {
     std::free(str);
@@ -44,7 +40,7 @@ char* matveev::getLine(std::istream& in, size_t& len)
 {
   size_t cap = 16;
   len = 0;
-  char* buf = static_cast< char* >(std::malloc(cap));
+  char* buf = reinterpret_cast< char* >(std::malloc(cap));
   if (buf == nullptr)
   {
     return nullptr;
@@ -60,7 +56,7 @@ char* matveev::getLine(std::istream& in, size_t& len)
     if (len + 1 >= cap)
     {
       size_t newCap = cap * 2;
-      char* newBuf = static_cast< char* >(std::malloc(newCap));
+      char* newBuf = reinterpret_cast< char* >(std::malloc(newCap));
       if (newBuf == nullptr)
       {
         std::free(buf);
@@ -89,8 +85,10 @@ char* matveev::getLine(std::istream& in, size_t& len)
 char* matveev::rmLat(char* dest, const char* src)
 {
   size_t j = 0;
-  for (size_t i = 0; src[i] != '\0'; ++i) {
-    if (!std::isalpha(static_cast< unsigned char >(src[i]))) {
+  for (size_t i = 0; src[i] != '\0'; ++i)
+  {
+    if (!std::isalpha(reinterpret_cast< unsigned char >(src[i])))
+    {
       dest[j] = src[i];
       j++;
     }
@@ -101,9 +99,12 @@ char* matveev::rmLat(char* dest, const char* src)
 
 int matveev::hasRep(const char* str)
 {
-  for (size_t i = 0; str[i] != '\0'; ++i) {
-    for (size_t j = i + 1; str[j] != '\0'; ++j) {
-      if (str[i] == str[j]) {
+  for (size_t i = 0; str[i] != '\0'; ++i)
+  {
+    for (size_t j = i + 1; str[j] != '\0'; ++j)
+    {
+      if (str[i] == str[j])
+      {
         return 1;
       }
     }
