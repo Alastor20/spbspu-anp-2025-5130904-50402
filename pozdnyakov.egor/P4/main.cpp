74a6a593251ecf533f58cee8c94fbb440305dcc1
R0me0Star <eip_spb@mail.ru>
2026-01-22 23:31:43 +0300

    code overhaul

--- pozdnyakov.egor/P4/main.cpp
+++ pozdnyakov.egor/P4/main.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
-#include <cctype>
-#include <cstring>
 #include <stdexcept>
+#include <new>
 
 namespace pozdnyakov
 {
   constexpr size_t ALPHABET_SIZE = 26;
 
-  int isSeparator(int ch)
+  bool isAlpha(char ch)
   {
-    return std::isspace(ch);
+    return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z');
+  }
+
+  char toLower(char ch)
+  {
+    if (ch >= 'A' && ch <= 'Z')
+    {
+      return ch + ('a' - 'A');
+    }
+    return ch;
+  }
+
+  size_t getLength(const char* str)
+  {
+    size_t len = 0;
+    while (str[len] != '\0')
+    {
+      ++len;
+    }
+    return len;
+  }
+
+  bool isSeparator(char ch)
+  {
+    return ch == ' ' || ch == '\t' || ch == '\v' || ch == '\f' || ch == '\r';
   }
 
   void fillCharMap(const char* str, bool* map)
   {
     for (size_t i = 0; str[i] != '\0'; ++i)
     {
-      if (std::isalpha(static_cast< unsigned char >(str[i])))
+      if (isAlpha(str[i]))
       {
-        unsigned char uCh = static_cast< unsigned char >(str[i]);
-        char lowerCh = static_cast< char >(std::tolower(uCh));
+        char lowerCh = toLower(str[i]);
         map[lowerCh - 'a'] = true;
       }
     }
@@ -34,43 +56,42 @@ namespace pozdnyakov
     delete[] words;
   }
 
-  char* readWord(std::istream& in, int (*check)(int))
+  char* readWord(std::istream& in, bool (*check)(char))
   {
     size_t capacity = 16;
     size_t size = 0;
-    char* str = new char[capacity];
+    char* str = nullptr;
     char ch;
 
     while (in.get(ch))
     {
       if (ch == '\n')
       {
-        delete[] str;
         return nullptr;
       }
-
       if (!check(ch))
       {
-        str[size] = ch;
-        ++size;
+        in.unget();
         break;
       }
     }
 
-    if (size == 0)
+    if (!in)
     {
-      delete[] str;
       return nullptr;
     }
 
+    str = new char[capacity];
+
     while (in.get(ch))
     {
+      if (ch == '\n')
+      {
+        in.unget();
+        break;
+      }
       if (check(ch))
       {
-        if (ch == '\n')
-        {
-          in.putback(ch);
-        }
         break;
       }
 
@@ -78,7 +99,6 @@ namespace pozdnyakov
       {
         size_t newCapacity = capacity * 2;
         char* newStr = nullptr;
-
         try
         {
           newStr = new char[newCapacity];
@@ -88,12 +108,10 @@ namespace pozdnyakov
           delete[] str;
           throw;
         }
-
         for (size_t i = 0; i < size; ++i)
         {
           newStr[i] = str[i];
         }
-
         delete[] str;
         str = newStr;
         capacity = newCapacity;
@@ -103,8 +121,48 @@ namespace pozdnyakov
       ++size;
     }
 
+    if (size == 0)
+    {
+      delete[] str;
+      return nullptr;
+    }
+
     str[size] = '\0';
     return str;
+  }
+
+  char** inputString(std::istream& in, size_t& count)
+  {
+    char** words = nullptr;
+    char* currentWord = nullptr;
+    count = 0;
+
+    while ((currentWord = readWord(in, isSeparator)) != nullptr)
+    {
+      char** newWords = nullptr;
+      try
+      {
+        newWords = new char*[count + 1];
+      }
+      catch (const std::bad_alloc&)
+      {
+        delete[] currentWord;
+        cleanupWords(words, count);
+        throw;
+      }
+
+      for (size_t i = 0; i < count; ++i)
+      {
+        newWords[i] = words[i];
+      }
+
+      newWords[count] = currentWord;
+      ++count;
+
+      delete[] words;
+      words = newWords;
+    }
+    return words;
   }
 
   void replaceChars(const char* input, char* output, char oldChar, char newChar)
@@ -128,7 +186,6 @@ namespace pozdnyakov
   void mergeLatinLetters(const char* s1, const char* s2, char* dest)
   {
     bool present[ALPHABET_SIZE];
-
     for (size_t k = 0; k < ALPHABET_SIZE; ++k)
     {
       present[k] = false;
@@ -159,41 +216,15 @@ int main()
 
   try
   {
-    while (true)
-    {
-      char* currentWord = readWord(std::cin, isSeparator);
-      if (currentWord == nullptr)
-      {
-        break;
-      }
-
-      char** newWords = nullptr;
-      try
-      {
-        newWords = new char* [wordsCount + 1];
-      }
-      catch (const std::bad_alloc&)
-      {
-        delete[] currentWord;
-        throw;
-      }
-
-      for (size_t i = 0; i < wordsCount; ++i)
-      {
-        newWords[i] = words[i];
-      }
-
-      newWords[wordsCount] = currentWord;
-      ++wordsCount;
-
-      delete[] words;
-      words = newWords;
-    }
+    words = inputString(std::cin, wordsCount);
   }
   catch (const std::bad_alloc&)
   {
     std::cerr << "Memory allocation error\n";
-    cleanupWords(words, wordsCount);
+    if (words != nullptr)
+    {
+      cleanupWords(words, wordsCount);
+    }
     return 1;
   }
 
@@ -215,7 +246,7 @@ int main()
     for (size_t i = 0; i < wordsCount; ++i)
     {
       char* currentWord = words[i];
-      size_t len = std::strlen(currentWord);
+      size_t len = getLength(currentWord);
 
       result1 = new char[len + 1];
       result1[len] = '\0';
@@ -235,18 +266,12 @@ int main()
   catch (const std::bad_alloc&)
   {
     std::cerr << "Memory allocation error\n";
-    if (result1)
-    {
-      delete[] result1;
-    }
-    if (result2)
-    {
-      delete[] result2;
-    }
+    delete[] result1;
+    delete[] result2;
     cleanupWords(words, wordsCount);
     return 1;
   }
-
   cleanupWords(words, wordsCount);
+
   return 0;
 }
